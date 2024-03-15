b9735552f96f4ea75c96a1a305c69f2fe316ed5d
Kaito117 <pranshuraj65536@gmail.com>
2024-03-15 12:19:03 +0530

    added measures for data homogeneity, updated data directories

--- src/processing.py
+++ src/processing.py
@@ -2,8 +2,11 @@ import os
 import pandas as pd
 
 
-directories = ['data/indeed/2024_03_11']
-required_columns = ['title', 'company', 'location', 'link', 'date', 'query', 'source']
+directories = ['data/raw/indeed/', 'data/raw/yc']
+required_columns = set(['title', 'company', 'salary', 'location', 'link', 'date', 'query', 'source'])
+
+# TODO: add aggregation of jobs - return a single csv file for all
+
 
 def get_paths(directories):
     '''
@@ -20,24 +23,38 @@ def get_data(path):
     return df
 
 
+def make_clickable(val):
+    return f'<a target="_blank" href="{val}">{"apply"}</a>'
+
+
 def process_data(data, path, source):
     data = data.drop_duplicates()
-    filename = str(os.path.basename(path))[:-4]
-    data['query'] = ' '.join(filename.split('_'))
-    data['link'] = data['link'].apply(lambda x: 'https://www.ycombinator.com/jobs/role' + str(x))
-    # add source hostname to link
     data['source'] = source
+    if source =='indeed':
+        data['link']=data['link'].apply(lambda x:'https://in.indeed.com' + str(x))
+    elif source =='yc':
+        data['link'] = data['link'].apply(lambda x: 'https://www.ycombinator.com/jobs/role' + str(x))
+    # add source hostname to link
+    
+    data.style.format({'link':make_clickable})
+    columns = set(data.columns)
+    if columns!=required_columns:
+        absent_columns = required_columns-columns
+        if 'salary' in absent_columns:
+            data['salary']='Not Specified'
+
+    if 'duration' in columns:
+        data=data.drop(columns=['duration'])
     return data
 
 # TODO : parse dates, remove jobs older than 20 days or so
+# TODO : if all required columns not present, init with empty values
 
-def save_data(data, path):
-    data.to_csv(path, index=False)
 
 if __name__ == '__main__':
     for path in get_paths(directories):
         data = get_data(path)
         source = str(os.path.dirname(os.path.dirname(path))).split('data/')[1]
         data = process_data(data, path, source)
-        path = str(path).replace(source, f'cleaned_{source}')
-        save_data(data, path)
+        path = str(path).replace('raw', 'cleaned')
+        data.to_csv(path, index=False)
